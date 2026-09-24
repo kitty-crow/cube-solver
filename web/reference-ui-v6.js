ba13a9e1dc1f50d1fd653546aeb2c39751ee5d84
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-24 17:18:16 +0100

    Use cross-face continuation aligner

--- web/reference-ui-v6.js
+++ web/reference-ui-v6.js
@@ -1,5 +1,5 @@
 import { ReferenceAssistant as PersistedReferenceAssistant } from "./reference-ui-v4.js";
-import { ReferenceAlignmentModal } from "./reference-aligner-v10.js";
+import { ReferenceAlignmentModal } from "./reference-aligner-v11.js";
 
 export class ReferenceAssistant extends PersistedReferenceAssistant{
   constructor(options={}){
