29e77f8da83a0fd365da2350504db3d85cbf2b8d
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-26 11:15:49 +0100

    Use refined sticker identification modal

--- web/reference-ui-v6.js
+++ web/reference-ui-v6.js
@@ -1,6 +1,6 @@
 import { ReferenceAssistant as PersistedReferenceAssistant } from "./reference-ui-v4.js";
 import { ReferenceAlignmentModal } from "./reference-aligner-v13.js";
-import { StickerIdentificationModal } from "./sticker-identification-ui-v7.js";
+import { StickerIdentificationModal } from "./sticker-identification-ui-v8.js";
 import { loadReferenceSession, saveReferenceSession } from "./reference-session.js";
 
 export class ReferenceAssistant extends PersistedReferenceAssistant{
